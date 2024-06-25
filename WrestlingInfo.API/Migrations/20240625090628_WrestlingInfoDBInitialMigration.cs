b63ccb0586cec528cff5ac2c979d0b60e1a0c713
FabienDumont <fabdum19@live.fr>
2024-06-25 11:14:02 +0200

    Added migrations

---
+++ WrestlingInfo.API/Migrations/20240625090628_WrestlingInfoDBInitialMigration.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WrestlingInfo.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class WrestlingInfoDBInitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Promotions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Promotions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wrestlers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wrestlers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WrestlingEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Date = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    PromotionId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WrestlingEvents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WrestlingEvents_Promotions_PromotionId",
+                        column: x => x.PromotionId,
+                        principalTable: "Promotions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WrestlingEventReviews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Rating = table.Column<double>(type: "REAL", nullable: false),
+                    Comment = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: true),
+                    WrestlingEventId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WrestlingEventReviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WrestlingEventReviews_WrestlingEvents_WrestlingEventId",
+                        column: x => x.WrestlingEventId,
+                        principalTable: "WrestlingEvents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Promotions",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "WWE" });
+
+            migrationBuilder.InsertData(
+                table: "Wrestlers",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "AJ Styles" },
+                    { 2, "Akira Tozawa" },
+                    { 3, "Akam" },
+                    { 4, "Alba Fyre" },
+                    { 5, "Andrade" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "WrestlingEvents",
+                columns: new[] { "Id", "Date", "Name", "PromotionId" },
+                values: new object[] { 1, new DateOnly(2024, 1, 1), "Raw", 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WrestlingEventReviews_WrestlingEventId",
+                table: "WrestlingEventReviews",
+                column: "WrestlingEventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WrestlingEvents_PromotionId",
+                table: "WrestlingEvents",
+                column: "PromotionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Wrestlers");
+
+            migrationBuilder.DropTable(
+                name: "WrestlingEventReviews");
+
+            migrationBuilder.DropTable(
+                name: "WrestlingEvents");
+
+            migrationBuilder.DropTable(
+                name: "Promotions");
+        }
+    }
+}
